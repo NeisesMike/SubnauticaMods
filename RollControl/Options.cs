ce82b65d1ce5f11d6a94b1724b76505690b8565f
Michael Neises <neisesmichael@gmail.com>
2020-07-12 20:35:03 -0500

    add options

---
+++ RollControl/Options.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Harmony;
+using SMLHelper.V2.Options;
+using SMLHelper.V2.Handlers;
+using LitJson;
+
+namespace RollControl
+{
+    internal struct OptionsObject
+    {
+        public KeyCode RollPortKey { get; set; }
+        public KeyCode RollStarboardKey { get; set; }
+        public float RollSpeed { get; set; }
+    }
+
+
+    public class Options : ModOptions
+    {
+        public KeyCode rollToPortKey = KeyCode.PageUp;
+        public KeyCode rollToStarboardKey = KeyCode.PageUp;
+        public float rollSpeed = 0.3F;
+
+        private string ConfigPath => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json");
+
+        public Options() : base("Roll Controls")
+        {
+            InitEvents();
+            LoadDefaults();
+        }
+
+        private void InitEvents()
+        {
+            KeybindChanged += Options_KeybindChanged;
+            SliderChanged += Options_SliderChanged;
+        }
+
+        private void Options_KeybindChanged(object sender, KeybindChangedEventArgs e)
+        {
+            switch (e.Id)
+            {
+                case "roll_to_port":
+                    rollToPortKey = e.Key;
+                    break;
+                case "roll_to_starboard":
+                    rollToStarboardKey = e.Key;
+                    break;
+            }
+            UpdateJSON();
+        }
+        private void Options_SliderChanged(object sender, SliderChangedEventArgs e)
+        {
+            switch (e.Id)
+            {
+                case "roll_speed":
+                    rollSpeed = e.Value;
+                    break;
+            }
+            UpdateJSON();
+        }
+
+        private void LoadDefaults()
+        {
+            if (!File.Exists(ConfigPath))
+            {
+                UpdateJSON();
+            }
+            else
+            {
+                ReadOptionsFromJSON();
+            }
+        }
+
+        private void UpdateJSON()
+        {
+            OptionsObject options = new OptionsObject
+            {
+                RollPortKey = rollToPortKey,
+                RollStarboardKey = rollToStarboardKey,
+                RollSpeed = rollSpeed
+            };
+
+            var stringBuilder = new StringBuilder();
+            var jsonWriter = new JsonWriter(stringBuilder)
+            {
+                PrettyPrint = true
+            };
+            JsonMapper.ToJson(options, jsonWriter);
+
+            string optionsJSON = stringBuilder.ToString();
+            File.WriteAllText(ConfigPath, optionsJSON);
+        }
+
+        private void ReadOptionsFromJSON()
+        {
+            if (File.Exists(ConfigPath))
+            {   // Parse and load options from the new config.json
+                try
+                {
+                    string optionsJSON = File.ReadAllText(ConfigPath);
+                    var options = JsonMapper.ToObject<OptionsObject>(optionsJSON);
+                    var data = JsonMapper.ToObject(optionsJSON);
+
+                    rollToPortKey = data.ContainsKey("RollPortKey") ? options.RollPortKey : rollToPortKey;
+                    rollToStarboardKey = data.ContainsKey("RollStarboardKey") ? options.RollStarboardKey : rollToStarboardKey;
+                    rollSpeed = data.ContainsKey("RollSpeed") ? options.RollSpeed : rollSpeed;
+
+                    //if (!data.ContainsKey("RollPortKey") || !data.ContainsKey("RollStarboardKey") || !data.ContainsKey("RollSpeed"))
+                    {
+                        UpdateJSON();
+                    }
+                }
+                catch (Exception)
+                {   // JSON was invalid, create default values
+                    UpdateJSON();
+                }
+            }
+            else
+            {   // Create the config.json with default values
+                UpdateJSON();
+            }
+        }
+
+        public override void BuildModOptions()
+        {
+            AddKeybindOption("roll_to_port", "Roll-to-Port Key", GameInput.GetPrimaryDevice(), rollToPortKey);
+            AddKeybindOption("roll_to_starboard", "Roll-to-Starboard Key", GameInput.GetPrimaryDevice(), rollToStarboardKey);
+            AddSliderOption("roll_speed", "Roll Speed", 0F, 1F, rollSpeed);
+        }
+    }
+
+}
