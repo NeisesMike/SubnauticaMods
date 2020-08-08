efef51fad460441b64e721ce4ffd419228694635
Michael Neises <neisesmichael@gmail.com>
2020-08-07 19:31:10 -0500

    init controller work

--- RollControl/GameInputPatcher.cs
+++ RollControl/GameInputPatcher.cs
@@ -9,34 +9,52 @@ using Harmony;
 using SMLHelper.V2.Options;
 using SMLHelper.V2.Handlers;
 using LitJson;
+using System.Runtime.CompilerServices;
 
 namespace RollControl
 {
-    [HarmonyPatch(typeof(GameInput))]
-    [HarmonyPatch("Initialise")]
+    public class InputManager
+    {
+        //GameInput.AnalogAxis portRoll;
+        string portRollAxis;
+        string starboardRollAxis;
 
+         public InputManager()
+        {
+            string portRollAxis = "ControllerAxis1";
+            string starboardRollAxis = "ControllerAxis2";
+        }
+    }
+
+    [HarmonyPatch(typeof(GameInput))]
+    [HarmonyPatch("Awake")]
     public class GameInputPatcher
     {
+        public static InputManager myInputMan;
 
-        /*ref PilotingChair chair,*/
         [HarmonyPrefix]
         public static bool Prefix(GameInput __instance)
         {
+            // initialize the roll manager
+            myInputMan = new InputManager();
+
             return true;
         }
 
         [HarmonyPostfix]
         public static void Postfix(GameInput __instance)
         {
-		// rotations
-		GameInput.AddAxisInput("Pitch", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
-		GameInput.AddAxisInput("Roll", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
-		GameInput.AddAxisInput("Yaw", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+            /*
+		    // rotations
+		    GameInput.AddAxisInput("Pitch", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+		    GameInput.AddAxisInput("Roll", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+		    GameInput.AddAxisInput("Yaw", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
 
-		// translations
-		GameInput.AddAxisInput("Vertical Thrust", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
-		GameInput.AddAxisInput("Forward Thrust", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
-		GameInput.AddAxisInput("Sideways Thrust", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+		    // translations
+		    GameInput.AddAxisInput("Vertical Thrust", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+		    GameInput.AddAxisInput("Forward Thrust", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+		    GameInput.AddAxisInput("Sideways Thrust", GameInput.AnalogAxis.ControllerLeftTrigger, true, GameInput.Device.Controller, 0f);
+            */
         }
     }
 }
