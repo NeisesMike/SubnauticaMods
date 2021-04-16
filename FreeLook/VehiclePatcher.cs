56d08597f74e41e6edf9e804badf37c6be82034a
Michael Neises <neisesmichael@gmail.com>
2021-04-16 10:10:18 -0500

    add xbox controller support

--- FreeLook/VehiclePatcher.cs
+++ FreeLook/VehiclePatcher.cs
@@ -8,11 +8,38 @@ using UnityEngine;
 using Harmony;
 using SMLHelper.V2.Options;
 using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
 using LitJson;
 using System.Net.NetworkInformation;
 
 namespace FreeLook
 {
+    public class FreeLookManager
+    {
+        public bool isFreeLooking = false;
+
+        public FreeLookManager()
+        {
+            isFreeLooking = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Vehicle))]
+    [HarmonyPatch("Awake")]
+    public class VehicleAwakePatcher
+    {
+        public static FreeLookManager myFreeMan;
+
+        [HarmonyPrefix]
+        public static bool Prefix(Vehicle __instance)
+        {
+            // initialize the roll manager
+            myFreeMan = new FreeLookManager();
+            return true;
+        }
+    }
+
+
     [HarmonyPatch(typeof(Vehicle))]
     [HarmonyPatch("Update")]
 
@@ -33,10 +60,16 @@ namespace FreeLook
 
         public static void moveCamera(Vehicle mySeamoth)
         {
-            Vector2 vector = GameInput.GetLookDelta();
+            Vector2 myLookDelta = GameInput.GetLookDelta();
+            if (myLookDelta == Vector2.zero)
+            {
+                myLookDelta.x = Input.GetAxis("ControllerAxis4");
+                myLookDelta.y = -Input.GetAxis("ControllerAxis5");
+            }
+
             MainCameraControl mainCam = MainCameraControl.main;
-            mainCam.rotationX += vector.x;
-            mainCam.rotationY += vector.y;
+            mainCam.rotationX += myLookDelta.x;
+            mainCam.rotationY += myLookDelta.y;
             mainCam.rotationX = Mathf.Clamp(mainCam.rotationX, -100, 100);
             mainCam.rotationY = Mathf.Clamp(mainCam.rotationY, mainCam.minimumY, mainCam.maximumY);
 
@@ -54,42 +87,133 @@ namespace FreeLook
         static float smoothTime     = 0.25f;
 
         static bool releaseFlag = false;
+        static Vector3 cameraOffsetTransformPosition = new Vector3(0, 0, 0);
+
+        static bool isTriggerDown = false;
+        static bool isTriggerNewlyDown = false;
+        static bool isTriggerNewlyUp = false;
+
+        static bool isInVehicle = false;
+        static bool isNewlyInVehicle = false;
 
         [HarmonyPrefix]
         public static bool Prefix(Vehicle __instance)
         {
             var mainCam = MainCameraControl.main;
-            var cameraToPlayerMan = CameraToPlayerManager.main;
+            //var cameraToPlayerMan = CameraToPlayerManager.main;
 
-            if ( !(Player.main.inSeamoth || Player.main.inExosuit ) ) //|| Player.main.motorMode==Player.MotorMode.Seaglide) )
+            bool inVehicleNow = (Player.main.inSeamoth || Player.main.inExosuit);
+
+            if(isInVehicle)
             {
-                if(releaseFlag)
+                if (inVehicleNow)
+                {
+                    // do nothing
+                }
+                else
+                {
+                    isInVehicle = false;
+                }
+            }
+            else
+            {
+                if (inVehicleNow)
+                {
+                    isInVehicle = true;
+                    isNewlyInVehicle = true;
+                }
+                else
+                {
+                    // do nothing
+                }
+            }
+
+            if(!inVehicleNow)
+            {
+                if (releaseFlag)
                 {
                     cameraRelinquish();
                     releaseFlag = false;
                 }
-
                 return true;
             }
             releaseFlag = true;
 
-            if(Player.main.motorMode == Player.MotorMode.Seaglide)
+            if(isNewlyInVehicle)
+            {
+                isNewlyInVehicle = false;
+                //BasicText message = new BasicText();
+                //message.ShowMessage("This Message Will Fade In 10 Seconds", 10);
+            }
+
+
+
+            if (Player.main.motorMode == Player.MotorMode.Seaglide)
             {
                 // hack the controls
-
             }
+
+            bool triggerState = (Input.GetAxisRaw("ControllerAxis3") > 0) || (Input.GetAxisRaw("ControllerAxis3") < 0);
+            if(isTriggerDown)
+            {
+                if(triggerState)
+                {
+                    //do nothing
+                }
+                else
+                {
+                    isTriggerDown = false;
+                    isTriggerNewlyUp = true;
+                }
+            }
+            else
+            {
+                if (triggerState)
+                {
+                    isTriggerDown = true;
+                    isTriggerNewlyDown = true;
+                }
+                else
+                {
+                    //do nothing
+                }
+            }
+
+            // add locomotion back in
+            if ((Input.GetKey(Options.freeLookKey) || isTriggerDown) && __instance == Player.main.currentMountedVehicle)// and we're using controller)
+            {
+                Vector3 myDirection = Vector3.zero;
+                myDirection.z = Input.GetAxis("ControllerAxis1");
+                myDirection.x = -Input.GetAxis("ControllerAxis2");
+                myDirection.y =
+                    GameInput.GetButtonHeld(GameInput.Button.MoveUp) ?
+                    (GameInput.GetButtonHeld(GameInput.Button.MoveDown) ? 0 : 1) :
+                    (GameInput.GetButtonHeld(GameInput.Button.MoveDown) ? -1 : 0);
+
+                Vector3 myModDir = __instance.transform.forward * myDirection.x +
+                                   __instance.transform.right   * myDirection.z +
+                                   __instance.transform.up      * myDirection.y;
+
+                myModDir = Vector3.Normalize(myModDir);
+
+                __instance.GetComponent<Rigidbody>().velocity += myModDir * Time.deltaTime * 10f;
+                __instance.GetComponent<Rigidbody>().velocity = Vector3.ClampMagnitude(__instance.GetComponent<Rigidbody>().velocity, 10f);
+            }
+
 
             void cameraRelinquish()
             {
                 mainCam.ResetCamera();
                 mainCam.cinematicMode = false;
                 mainCam.lookAroundMode = true;
-
+                VehicleAwakePatcher.myFreeMan.isFreeLooking = false;
             }
 
-            if (Input.GetKeyDown(Options.freeLookKey))
+            if (Input.GetKeyDown(Options.freeLookKey) || isTriggerNewlyDown)
             {
                 Debug.Log("FreeLook: button pressed. Taking control of the camera.");
+                VehicleAwakePatcher.myFreeMan.isFreeLooking = true;
+                isTriggerNewlyDown = false;
 
                 resetCameraFlag = false;
                 // invoke a camera vulnerability
@@ -97,12 +221,13 @@ namespace FreeLook
                 mainCam.lookAroundMode = false;
                 return false;
             }
-            else if (Input.GetKeyUp(Options.freeLookKey))
+            else if (Input.GetKeyUp(Options.freeLookKey) || isTriggerNewlyUp)
             {
+                isTriggerNewlyUp = false;
                 Debug.Log("FreeLook: button released. Relinquishing control of the camera.");
                 resetCameraFlag = true;
             }
-            if ( !resetCameraFlag && Input.GetKey(Options.freeLookKey))
+            if ( !resetCameraFlag && (Input.GetKey(Options.freeLookKey) || isTriggerDown) )
             {
                 resetCameraFlag = false;
                 moveCamera(Player.main.currentMountedVehicle);
