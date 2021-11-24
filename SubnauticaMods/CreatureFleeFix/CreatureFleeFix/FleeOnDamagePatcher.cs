c82383b30e0987a55d97610616cab884ca9b466e
Michael Neises <neisesmichael@gmail.com>
2021-11-23 18:09:17 -0600

    add initial flee fix mod

---
+++ SubnauticaMods/CreatureFleeFix/CreatureFleeFix/FleeOnDamagePatcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using HarmonyLib;
+using System.Runtime.CompilerServices;
+using System.Collections;
+
+namespace CreatureFleeFix
+{
+	[HarmonyPatch(typeof(FleeOnDamage))]
+	[HarmonyPatch("OnTakeDamage")]
+	class OnTakeDamagePatcher
+	{
+		[HarmonyPostfix]
+		public static void Postfix(FleeOnDamage __instance, DamageInfo damageInfo)
+		{
+			if (__instance.accumulatedDamage > __instance.damageThreshold)
+			{
+				Vector3 DamageDealerPosition = damageInfo.position;
+				if(DamageDealerPosition == Vector3.zero)
+				{
+					// the chance that we actually meant to calculate (0,0,0) is neglible,
+					// so let's flee from the player instead,
+					DamageDealerPosition = Player.main.transform.position;
+				}
+				Vector3 MoveVector = Vector3.Normalize(__instance.transform.position - DamageDealerPosition) * (__instance.minFleeDistance + damageInfo.damage / 30f);
+				Vector3 DestinationVector = MoveVector + __instance.transform.position;
+				Vector3 DestinationVectorOcean = new Vector3(DestinationVector.x, Mathf.Min(DestinationVector.y, Ocean.main.GetOceanLevel()), DestinationVector.z);
+				__instance.moveTo = DestinationVectorOcean;
+				__instance.timeToFlee = Time.time + __instance.fleeDuration;
+			}
+		}
+	}
+}
---
+++ SubnauticaMods/CreatureFleeFix/CreatureFleeFix/MainPatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Utility;
+
+namespace CreatureFleeFix
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log("[CreatureFleeFix] " + message);
+        }
+        public static void Output(string msg)
+        {
+            BasicText message = new BasicText(500, 0);
+            message.ShowMessage(msg, 5);
+        }
+    }
+    [QModCore]
+    public static class MainPatcher
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            var harmony = new Harmony("com.mikjaw.subnautica.creaturefleefix.mod");
+            harmony.PatchAll();
+        }
+    }
+}
