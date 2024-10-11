5240e4e8eb82a65bbde55a1c5d7aa5a31209fc02
Michael Neises <neisesmichael@gmail.com>
2024-10-11 17:40:07 -0500

    update drillarm plugininfo

--- SubnauticaMods/VehicleFrameworkUpgradeModules/ModVehicleArms/MainPatcher.cs
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/ModVehicleArms/MainPatcher.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 namespace VFDrillArm
 {
-    [BepInPlugin("com.mikjaw.subnautica.vfdrillarm.mod", "VFDrillArm", "1.1")]
+    [BepInPlugin("com.mikjaw.subnautica.vfdrillarm.mod", "VFDrillArm", "1.3")]
     [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID)]
     [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
     public class MainPatcher : BaseUnityPlugin
