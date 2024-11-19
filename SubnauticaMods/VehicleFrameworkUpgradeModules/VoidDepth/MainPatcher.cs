b2b9a4b9d1e5be02b5cff74b4aeb86ae01b46e77
Michael Neises <neisesmichael@gmail.com>
2024-11-19 03:46:35 -0600

    Add void depth upgrade

---
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/VoidDepth/MainPatcher.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace VoidDepth
+{
+    [BepInPlugin("com.mikjaw.subnautica.voiddepth.mod", "VoidDepthUpgrade", "1.0")]
+    [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
+    public class MainPatcher : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            VehicleFramework.Admin.UpgradeRegistrar.RegisterUpgrade(new VoidDepth());
+        }
+    }
+}
---
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/VoidDepth/VoidDepth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using VehicleFramework.UpgradeTypes;
+using VehicleFramework.Assets;
+
+namespace VoidDepth
+{
+    public class VoidDepth : ModVehicleUpgrade
+    {
+        public override string ClassId => "VoidDepthUpgrade";
+        public override string DisplayName => "Void Depth Module";
+        public override string Description => "Combines advanced alloys with the enigmatic properties of Ghost Weed to achieve extraordinary crush depth capabilities. Stacks.";
+        public override List<Ingredient> Recipe => new List<Ingredient>()
+        {
+            new Ingredient(TechType.Kyanite, 2),
+            new Ingredient(TechType.PlasteelIngot, 1),
+            new Ingredient(TechType.Nickel, 1),
+            new Ingredient(TechType.RedGreenTentacleSeed, 1),
+        };
+        public override Atlas.Sprite Icon => SpriteHelper.GetSprite("VoidDepthIcon.png");
+        public const float crushAddition = 5000;
+        public override void OnAdded(AddActionParams param)
+        {
+            param.vehicle.crushDamage.kBaseCrushDepth += crushAddition;
+            param.vehicle.crushDamage.UpdateDepthClassification();
+        }
+        public override void OnRemoved(AddActionParams param)
+        {
+            param.vehicle.crushDamage.kBaseCrushDepth -= crushAddition;
+            param.vehicle.crushDamage.UpdateDepthClassification();
+        }
+    }
+}
