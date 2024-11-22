69354a915688490eb0dfe90793cacab364c9be18
Michael Neises <neisesmichael@gmail.com>
2024-11-22 16:52:34 -0600

    Always stop FreeLooking on pilot exit

--- SubnauticaMods/FreeLook/PlayerPatcher.cs
+++ SubnauticaMods/FreeLook/PlayerPatcher.cs
@@ -5,12 +5,22 @@ namespace FreeLook
     [HarmonyPatch(typeof(Player))]
     public class PlayerStartPatcher
     {
-        [HarmonyPrefix]
-        [HarmonyPatch("Start")]
-        public static bool Prefix(Player __instance)
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Player.Start))]
+        public static void PlayerStartPostfix(Player __instance)
         {
             __instance.gameObject.EnsureComponent<FreeLookManager>();
-            return true;
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Player.ExitLockedMode))]
+        public static void PlayerExitLockedModePostfix(Player __instance)
+        {
+            if (__instance.GetMode() == Player.Mode.Normal)
+            {
+                FreeLookManager flm = __instance.gameObject.EnsureComponent<FreeLookManager>();
+                flm.isToggled = false;
+                flm.isFreeLooking = false;
+            }
         }
     }
 }
