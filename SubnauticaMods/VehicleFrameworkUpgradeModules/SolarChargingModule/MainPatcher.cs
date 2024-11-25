861c218baeb5635a2df6fcda0b7932d23c17ebaf
Michael Neises <neisesmichael@gmail.com>
2024-11-25 16:18:44 -0600

    update solar charger plugininfo

--- SubnauticaMods/VehicleFrameworkUpgradeModules/SolarChargingModule/MainPatcher.cs
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/SolarChargingModule/MainPatcher.cs
@@ -3,11 +3,12 @@ using Nautilus.Handlers;
 
 namespace SolarChargingModule
 {
-    [BepInPlugin("com.mikjaw.subnautica.solarchargingmodule.mod", "SolarChargingModule", "1.3")]
-    [BepInDependency("com.snmodding.nautilus")]
+    [BepInPlugin(pluginGUID, "SolarChargingModule", "1.3")]
+    [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID)]
     [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
     public class MainPatcher : BaseUnityPlugin
     {
+        public const string pluginGUID = "com.mikjaw.subnautica.solarchargingmodule.mod";
         internal static Config MyConfig { get; private set; }
         public void Start()
         {
