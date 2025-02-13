5316b3409c31e098c14071adf1e62c10c45a2379
Michael Neises <neisesmichael@gmail.com>
2025-02-12 19:55:01 -0600

    Allow disabling the prawn drill arm

---
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/ModVehicleArms/Config.cs
@@ -0,0 +1,17 @@
+﻿using Nautilus.Options.Attributes;
+using Nautilus.Json;
+
+namespace VFDrillArm
+{
+    [Menu("VF Drill Arm Options")]
+    public class Config : ConfigFile
+    {
+        const string optionText =
+            "Disable this to NOT have a Prawn version of this upgrade. " +
+            "This Prawn drill arm has the same recipe as the normal drill arm, " +
+            "but it's built at the Modification station and can be toggled. " +
+            "If you change this option, please restart the game for it to take effect!";
+        [Toggle("Prawn Arm", Tooltip = optionText)]
+        public bool isPrawnArm = true;
+    }
+}
--- SubnauticaMods/VehicleFrameworkUpgradeModules/ModVehicleArms/MainPatcher.cs
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/ModVehicleArms/MainPatcher.cs
@@ -3,14 +3,23 @@ using HarmonyLib;
 
 namespace VFDrillArm
 {
-    [BepInPlugin("com.mikjaw.subnautica.vfdrillarm.mod", "VFDrillArm", "1.6")]
+    [BepInPlugin("com.mikjaw.subnautica.vfdrillarm.mod", "VFDrillArm", "1.8")]
     [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID)]
     [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
     public class MainPatcher : BaseUnityPlugin
     {
+        internal static Config MyConfig { get; private set; }
         public void Start()
         {
-            VehicleFramework.Admin.UpgradeRegistrar.RegisterUpgrade(new VFDrillArm());
+            MyConfig = Nautilus.Handlers.OptionsPanelHandler.RegisterModOptions<Config>();
+            VehicleFramework.Admin.UpgradeCompat compat = new VehicleFramework.Admin.UpgradeCompat
+            {
+                skipCyclops = true,
+                skipModVehicle = false,
+                skipSeamoth = true,
+                skipExosuit = !MyConfig.isPrawnArm
+            };
+            VehicleFramework.Admin.UpgradeRegistrar.RegisterUpgrade(new VFDrillArm(), compat);
             var harmony = new Harmony("com.mikjaw.subnautica.vfdrillarm.mod");
             harmony.PatchAll();
         }
