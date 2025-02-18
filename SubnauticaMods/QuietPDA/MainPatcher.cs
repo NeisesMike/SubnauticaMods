98eeb5d24d9275de12a132c197712da8cbe6421f
Michael Neises <neisesmichael@gmail.com>
2025-02-18 17:01:21 -0600

    Add QuietPDA mod

---
+++ SubnauticaMods/QuietPDA/MainPatcher.cs
@@ -0,0 +1,25 @@
+﻿namespace QuietPDA
+{
+    [BepInEx.BepInPlugin("com.mikjaw.subnautica.quietpda.mod", "Quiet PDA", "1.0")]
+    public class MainPatcher : BepInEx.BaseUnityPlugin
+    {
+        public void Start()
+        {
+            new HarmonyLib.Harmony("com.mikjaw.subnautica.quietpda.mod").PatchAll();
+        }
+    }
+    [HarmonyLib.HarmonyPatch(typeof(SoundQueue))]
+    public class SoundQueuePatcher
+    {
+        [HarmonyLib.HarmonyTargetMethod]
+        public static System.Reflection.MethodBase TargetMethod()
+        {
+            return typeof(SoundQueue).GetMethod("PlayQueued", new System.Type[] { typeof(string), typeof(string) });
+        }
+        [HarmonyLib.HarmonyPrefix]
+        public static bool SoundQueuePlayQueuedHarmonyPrefix()
+        {
+            return false;
+        }
+    }
+}
