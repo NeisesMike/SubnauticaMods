d1c25dd43e85c4185fcdef538bb7884f09aad634
Michael Neises <neisesmichael@gmail.com>
2025-02-28 15:56:54 -0600

    jukebox: add commentary

--- SubnauticaMods/JukeboxLib/QuickSlotsPatcher.cs
+++ SubnauticaMods/JukeboxLib/QuickSlotsPatcher.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 namespace JukeboxLib
 {
+    // This patch allows us to use the scroll wheel (or next slot, prev slot)
+    // to control volume without actually changing our equipped item
     [HarmonyPatch(typeof(uGUI_QuickSlots))]
     public class uGUI_QuickSlotsPatcher
     {
@@ -11,7 +13,7 @@ namespace JukeboxLib
         public static bool HandleInputPrefix()
         {
             Targeting.GetTarget(Player.main.gameObject, 6f, out GameObject target, out float _);
-            if (target?.GetComponentInParent<Jukebox>() != null)
+            if (target?.GetComponentInParent<DesktopJukebox>() != null)
             {
                 return false;
             }
