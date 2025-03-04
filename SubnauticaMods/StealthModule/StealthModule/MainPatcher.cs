f34820e23dd52b5883bdd6fd586eee2a182b7a2c
Michael Neises <neisesmichael@gmail.com>
2025-03-03 19:41:22 -0600

    stealth module: don't use VF logger

--- SubnauticaMods/StealthModule/StealthModule/MainPatcher.cs
+++ SubnauticaMods/StealthModule/StealthModule/MainPatcher.cs
@@ -8,7 +8,7 @@ using HarmonyLib;
 
 namespace StealthModule
 { 
-    [BepInPlugin(pluginGUID, "StealthModule", "3.0.3")]
+    [BepInPlugin(pluginGUID, "StealthModule", "3.0.4")]
     [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
     [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID)]
     public class MainPatcher : BaseUnityPlugin
--- SubnauticaMods/StealthModule/StealthModule/StealthModuleLogger.cs
+++ SubnauticaMods/StealthModule/StealthModule/StealthModuleLogger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 using UnityEngine;
+using Nautilus.Utility;
 
 namespace StealthModule
 {
@@ -34,6 +35,22 @@ namespace StealthModule
             UWE.CoroutineHost.StartCoroutine(LogOnce());
 
         }
+        public static BasicText Output(string msg, float time = 4f, int x = 500, int y = 0)
+        {
+            bool flag = GUIController.main.GetHidePhase() < GUIController.HidePhase.HUD;
+            BasicText result;
+            if (flag)
+            {
+                BasicText basicText = new BasicText(x, y);
+                basicText.ShowMessage(msg, time);
+                result = basicText;
+            }
+            else
+            {
+                result = null;
+            }
+            return result;
+        }
         private IEnumerator LogOnce()
         {
             const float timeToWait = 1f;
@@ -41,7 +58,7 @@ namespace StealthModule
             var entryList = LogDict.Values.ToList();
             entryList.Sort();
             var entry = entryList.First();
-            VehicleFramework.Logger.Output(entry.name + Mathf.RoundToInt(entry.distance).ToString() + "m", time: timeToWait);
+            Output(entry.name + Mathf.RoundToInt(entry.distance).ToString() + "m", time: timeToWait);
             LogDict.Clear();
             yield return new WaitForSeconds(timeToWait);
             waiting = false;
