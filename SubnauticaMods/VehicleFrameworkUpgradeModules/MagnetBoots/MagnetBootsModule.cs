cc38265f7ff02a70bbba4d6786aec5ae8923ae74
Michael Neises <neisesmichael@gmail.com>
2025-04-08 14:37:49 -0500

    Initial magnetboots mod

---
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/MagnetBoots/MagnetBootsModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using VehicleFramework.UpgradeTypes;
+
+namespace MagnetBoots
+{
+    public class MagnetBootsModule : ModVehicleUpgrade
+    {
+        public override string ClassId => "MagnetBootsModule";
+        public override string DisplayName => "Magnet Boots Module";
+        public override List<VehicleFramework.Assets.Ingredient> Recipe => new List<VehicleFramework.Assets.Ingredient>()
+                {
+                    new VehicleFramework.Assets.Ingredient(TechType.Magnetite, 1),
+                    new VehicleFramework.Assets.Ingredient(TechType.Nickel, 1),
+                    new VehicleFramework.Assets.Ingredient(TechType.PrecursorIonCrystal, 1),
+                    new VehicleFramework.Assets.Ingredient(TechType.Polyaniline, 1)
+                };
+        public override string Description => "Gain the ability to magnetically attach the upgraded vehicle to vehicles and bases on its ventral side (below it).";
+        public override Atlas.Sprite Icon => VehicleFramework.Assets.SpriteHelper.GetSprite("MagnetBootsIcon.png");
+        public override void OnAdded(AddActionParams param)
+        {
+            UpdateMagnetBoots(param.vehicle);
+        }
+        public override void OnRemoved(AddActionParams param)
+        {
+            UpdateMagnetBoots(param.vehicle);
+        }
+        private void UpdateMagnetBoots(Vehicle vehicle)
+        {
+            var boots = vehicle.gameObject.EnsureComponent<VehicleFramework.VehicleComponents.MagnetBoots>();
+            boots.enabled = GetNumberInstalled(vehicle) > 0;
+            VehicleFramework.VehicleComponents.MagnetBoots.colliderPairsPerFrame = MainPatcher.MagnetConfig.pairsPerFrame;
+        }
+    }
+}
---
+++ SubnauticaMods/VehicleFrameworkUpgradeModules/MagnetBoots/MainPatcher.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using Nautilus.Options.Attributes;
+using Nautilus.Json;
+using Nautilus.Handlers;
+
+namespace MagnetBoots
+{
+    [BepInPlugin("com.mikjaw.subnautica.magnetboots.mod", "MagnetBoots", "1.0.0")]
+    [BepInDependency(VehicleFramework.PluginInfo.PLUGIN_GUID, VehicleFramework.PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID)]
+    public class MainPatcher : BaseUnityPlugin
+    {
+        internal static MagnetBootsConfig MagnetConfig { get; private set; }
+        public void Start()
+        {
+            VehicleFramework.Admin.UpgradeRegistrar.RegisterUpgrade(new MagnetBootsModule());
+            MagnetConfig = OptionsPanelHandler.RegisterModOptions<MagnetBootsConfig>();
+        }
+    }
+
+    [Menu("Magnet Boots Options")]
+    public class MagnetBootsConfig : ConfigFile
+    {
+        [Slider("Collider Pairs Per Frame (performance)", Tooltip = "When magnet boots are used, they ask for collisions between the vehicle and its host to be ignored. This can be very expensive because some vehicles and hosts have a large number of colliders. Raising this number will make the magnet-vehicle touchable sooner. Lowering this number will reduce stutter on magnet events. Set to zero for no limit.", DefaultValue = 20, Step = 1, Min = 0, Max = 100)]
+        public int pairsPerFrame = 20;
+    }
+}
