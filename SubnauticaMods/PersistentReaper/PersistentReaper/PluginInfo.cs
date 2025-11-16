1e2365bc60d6c7563e47d57244af76af4e3282d4
Michael Neises <neisesmichael@gmail.com>
2025-11-16 17:44:00 -0600

    Update PluginInfo.cs

--- SubnauticaMods/PersistentReaper/PersistentReaper/PluginInfo.cs
+++ SubnauticaMods/PersistentReaper/PersistentReaper/PluginInfo.cs
@@ -4,7 +4,7 @@ namespace PersistentReaper
     public static class PluginInfo
     {
         public const string PLUGIN_GUID = "com.mikjaw.subnautica.persistentreapers.mod";
-        public const string PLUGIN_NAME = "VehicleFramePersistentReaperswork";
+        public const string PLUGIN_NAME = "PersistentReapers";
         public const string PLUGIN_VERSION = "3.0.0";
     }
 }
